f19751fc4182b28075c072883c37b151147bcb29
falloshka <falloshka@gmail.com>
2026-05-22 17:41:30 +0200

    Add files via upload

--- api/img-validate.js
+++ api/img-validate.js
@@ -1,13 +1,17 @@
 import fetch from "node-fetch";
 
 export default async function handler(req, res) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
+  // ✅ CORS - FULL FIX
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "*");
+
+  // ✅ PREFLIGHT handle
   if (req.method === "OPTIONS") {
     return res.status(200).end();
   }
+
 
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Only POST allowed" });
